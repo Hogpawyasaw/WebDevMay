cdfe3979120b72e8fa3370fdce441f36a77d1c2c
Hogpawyasaw <alexponcinn@hotmail.com>
2022-06-10 15:53:49 +0200

    function ex updated

--- javascript/javascript ex/functionex.js
+++ javascript/javascript ex/functionex.js
@@ -1,51 +1,57 @@
 //BMI calculation
-//Simplified version
-function BMICalc1(){
-    var weight = 85;
-    var height = 183;
-    BMIOutput1 = weight / (height * height);
-    console.log(BMIOutput1);
-}
-BMICalc1()
-
-//More complex version
-//Regular version
-function BMICalc2(){
-    var user = {
-        name : "Jack",
-        age : 45,
-        weight : 85,
-        height : 183
-    };
-    BMICalculation = user.weight / (user.height * user.height);
-    BMIOutput2 = user.weight / (user.height * user.height) ? `Because ${user.name}'s height is ${user.height} and weight is ${user.weight} his BMI is ${BMICalculation}.`: `The information introduced is not correct.`
-    console.log(BMIOutput2);
- }
- BMICalc2()
- //Arrow function
-
-
+    //Simplified function
+        //Reguler function
+            function BMICalc1(){
+                var weight = 85;
+                var height = 183;
+                BMIOutput1 = weight / (height * height);
+                console.log(BMIOutput1);
+            }
+            BMICalc1()
+        //End regular function
+        //Arrow function
+        var nettoSalaryCalc2 = (n1, n2, n3) => n1/ (n2*n3)
+        console.log(85 / (183 * 183))
+        //End arrow function
+    //End simplified function
+    //More complex function
+        //Regular function
+            function BMICalc2(){
+                var user = {
+                    name : "Jack",
+                    age : 45,
+                    weight : 85,
+                    height : 183
+                };
+                BMICalculation2 = user.weight / (user.height * user.height);
+                BMIOutput2 = user.weight / (user.height * user.height) ? `Because ${user.name}'s height is ${user.height} and weight is ${user.weight} his BMI is ${BMICalculation2}.`: `The information introduced is not correct.`
+                console.log(BMIOutput2);
+            }
+            BMICalc2()
+        //End regular version
+    //End more complex version
 //End BMI calculation
 
 
 //Netto salary
-    //Simplified version
-        //Regular version
+    //Simplified function
+        //Regular function
             function nettoSalaryCalc1(){
                 bruto = 3500;
                 percent = 25;
                 nettoSalaryOutput1 = bruto * percent / 100;
                 console.log(nettoSalaryOutput1);
             }
-            nettoSalaryCalc1()
-        //End regular version
+            nettoSalaryCalc1();
+        //End function version
         //Arrow function
-
-        //End arrow function
+            var nettoSalaryCalc2 = (n1, n2, n3) => n1*n2/n3
+            console.log(3500*25/100)
+         //End arrow function
     //End simplified version
     //More complex version
-        //Regular version
-            function nettoSalaryCalc2(){
+        //Regular function
+            function nettoSalaryCalc3(){
                 var user = {
                     name : "Jack",
                     age : 45,
@@ -55,14 +61,11 @@ function BMICalc2(){
                         percent : 25
                     }
                 };
-                nettoSalaryCalculation = user.job.bruto * user.job.percent / 100;
-                nettoSalaryOutput2 = user.job.bruto * user.job.percent / 100 ? `${user.name}'s job as a ${user.job.type} gets him a bruto of ${user.job.bruto} he has to give ${user.job.percent}% back and ends up with a neto of ${nettoSalaryCalculation}.`: `The information introduced is not correct.`;
+                nettoSalaryCalculation3 = user.job.bruto * user.job.percent / 100;
+                nettoSalaryOutput2 = user.job.bruto * user.job.percent / 100 ? `${user.name}'s job as a ${user.job.type} gets him a bruto of ${user.job.bruto} he has to give ${user.job.percent}% back and ends up with a neto of ${nettoSalaryCalculation3}.`: `The information introduced is not correct.`;
                 console.log(nettoSalaryOutput2);
             }
-            nettoSalaryCalc2()
-        //End regular version
-        //Arrow function
-
-        //End arrow functio
+            nettoSalaryCalc3()
+        //End regular function
     //End more complex version
 //End netto salary
--- practice/javascript/functions1.js
+++ practice/javascript/functions1.js
@@ -1,5 +1,6 @@
 //Regular function
 
+//Return explanation (add return if you dont add data in your function)
 //Example
 // function getSum(){
 //     var getResult = 1;
@@ -9,8 +10,71 @@
 // getSum()
 // console.log(getSum())
 //End example
+//Example
+function getNumber1(){
+    console.log(5);
+    return 5
+};
+function getNumber2(){
+    console.log(6);
+    return 6
+};
+function getResult(){
+    console.log(getNumber1()+getNumber2());
+};
+// getResult()
+//End example
+//End return explanation
 
-function getSumOfNumbers(n1, n2){
-    console.log(n1, n2);
-}
-getSumOfNumbers
+//Example
+// function getSumOfNumbers(n1, n2){
+//     // console.log(n1, n2);
+//     return n1+n2
+// };
+// // getSumOfNumbers("hello world", [1,2,3]) 
+// // getSumOfNumbers({id:1}, null)
+// console.log(getSumOfNumbers(4,5));
+// console.log(getSumOfNumbers(10, 20));
+//End example
+
+//Function with parameter
+var student = {
+    name : "mal",
+    age : 23
+};
+function getStudentName(studentObj){
+    console.log(studentObj.name);
+};
+// getStudentName(student)
+//End function with parameter
+
+//Function with parameter
+var student = {
+    name : "Mal",
+    age: 23,
+    isStudent : true,
+    isTeacher : false,
+    scores : [10, 20, 30, 40]
+};
+function verifyStudent(studentParam){
+//  console.log(studentParam);
+ let isStudent = studentParam.isStudent
+//  console.log(isStudent);
+ return isStudent ? `NAME:${studentParam.name}\nAGE:${studentParam.age}\nSCORES:${studentParam.scores.toString()}` : "Is not a student"
+};
+
+//Arrow function 
+var verifyStudent2 = studentParam => {
+    //  console.log(studentParam);
+ let isStudent = studentParam.isStudent
+ //  console.log(isStudent);
+ console.log("Arrow function");
+  return isStudent ? `NAME:${studentParam.name}\nAGE:${studentParam.age}\nSCORES:${studentParam.scores.toString()}` : "Is not a student"
+ };
+var getSomeOfNumber2 = (n1, n2) => n1+n2
+
+//End arrow function
+console.log(getSomeOfNumber2(4, 5));
+//End function with parameter
+
+
